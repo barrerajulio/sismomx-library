7c390eb4ccdc1cf8fdf4bf890a15e52a6f95a88b
Omar Alvarado <oalvarado@conlana.com>
2017-09-23 15:52:39 -0500

    #2 HelpRequestDto created (Urgencias y solicitudes por zona)

---
+++ src/Dtos/HelpRequestDto.php
@@ -0,0 +1,54 @@
+<?php
+namespace CodeandoMexico\Sismomx\Core\Dtos;
+
+/**
+ * Class HelpRequestDto
+ * @package CodeandoMexico\Sismomx\Core\Dtos
+ */
+class HelpRequestDto
+{
+    /**
+     * @var string
+     */
+    public $urgencyLevel;
+
+    /**
+     * @var string
+     */
+    public $brigadeRequired;
+
+    /**
+     * @var string
+     */
+    public $mostImportantRequired;
+
+    /**
+     * @var string
+     */
+    public $admitted;
+
+    /**
+     * @var string
+     */
+    public $notRequired;
+
+    /**
+     * @var string
+     */
+    public $address;
+
+    /**
+     * @var string
+     */
+    public $zone;
+
+    /**
+     * @var string
+     */
+    public $source;
+
+    /**
+     * @var string
+     */
+    public $updatedAt;
+}
