4248afff86ab4f15b7285a8b0686077abd597497
Omar Alvarado <oalvarado@conlana.com>
2017-09-23 16:01:36 -0500

    #2 ShelterDto created (albergue)

---
+++ src/Dtos/ShelterDto.php
@@ -0,0 +1,44 @@
+<?php
+namespace CodeandoMexico\Sismomx\Core\Dtos;
+
+/**
+ * Class ShelterDto
+ * @package CodeandoMexico\Sismomx\Core\Dtos
+ */
+class ShelterDto
+{
+    /**
+     * @var string
+     */
+    public $location;
+
+    /**
+     * @var string
+     */
+    public $receiving;
+
+    /**
+     * @var string
+     */
+    public $address;
+
+    /**
+     * @var string
+     */
+    public $zone;
+
+    /**
+     * @var string
+     */
+    public $map;
+
+    /**
+     * @var string
+     */
+    public $moreInfo;
+
+    /**
+     * @var string
+     */
+    public $updatedAt;
+}
