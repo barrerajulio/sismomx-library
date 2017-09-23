18ecae80eaf04790fad2ff0f03f3ded19ecb6e05
Omar Alvarado <oalvarado@conlana.com>
2017-09-23 15:59:04 -0500

    #2 CollectionCenterDto created (Centro de acopio)

---
+++ src/Dtos/CollectionCenterDto.php
@@ -0,0 +1,54 @@
+<?php
+namespace CodeandoMexico\Sismomx\Core\Dtos;
+
+/**
+ * Class CollectionCenterDto
+ * @package CodeandoMexico\Sismomx\Core\Dtos
+ */
+class CollectionCenterDto
+{
+    /**
+     * @var string
+     */
+    public $urgencyLevel;
+
+    /**
+     * @var string
+     */
+    public $location;
+
+    /**
+     * @var string
+     */
+    public $requirementsDetails;
+
+    /**
+     * @var string
+     */
+    public $address;
+
+    /**
+     * @var string
+     */
+    public $zone;
+
+    /**
+     * @var string
+     */
+    public $map;
+
+    /**
+     * @var string
+     */
+    public $moreInformation;
+
+    /**
+     * @var string
+     */
+    public $updatedAt;
+
+    /**
+     * @var string
+     */
+    public $contact;
+}
