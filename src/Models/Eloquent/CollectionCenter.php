18c7dfd40452d7f500a8454f4d796ff879c6b029
Fabián JL <fabian.main@gmail.com>
2017-09-23 22:05:15 -0500

    #4 Definicion de modelo y repositorio para tabla collection_center

---
+++ src/Models/Eloquent/CollectionCenter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CodeandoMexico\Sismomx\Core\Models\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * User: @fabianjuarezmx
+ * Date: 9/23/17
+ */
+class CollectionCenter extends Model
+{
+
+}
---
+++ src/Repositories/Eloquent/CollectionCenterRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CodeandoMexico\Sismomx\Core\Repositories\Eloquent;
+
+use CodeandoMexico\Sismomx\Core\Models\Eloquent\CollectionCenter;
+
+/**
+ * User: @fabianjuarezmx
+ * Date: 9/23/17
+ */
+class CollectionCenterRepository extends BaseRepository
+{
+    /**
+     * @var CollectionCenter
+     */
+    protected $model;
+
+    /**
+     * CollectionCenterRepository constructor.
+     * @param CollectionCenter $model
+     */
+    public function __construct(CollectionCenter $model)
+    {
+        $this->model = $model;
+    }
+}
